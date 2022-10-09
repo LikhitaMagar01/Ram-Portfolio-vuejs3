db0b4b7c971d13ae1e13d7cbcf1ccedbcc4aafa4
LikhitaMagar01 <likhita.magar@patancollege.edu.np>
2022-10-09 18:32:48 +0545

    home,about page done

---
+++ src/components/About.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <div class="grid grid-cols-1 place-items-center">
+            <div class="text-2xl font-bold mt-20">
+               About me 
+            </div>
+            
+            <span class="text-red-500 mb-20"> -- who i am --</span>
+        </div>
+        <div class="grid grid-cols-2 mb-52">
+            <div class="pl-40">
+                <img src="/src/assets/Profile.png" alt="photo">
+            </div>
+            
+            <div>
+                <h1 class="text-2xl font-bold mb-10 mt-40">
+                   I am Ram Kumar Mahato and I'm a Web Developer 
+                </h1>
+                <p class="mr-20 mb-10 text-justify">
+                    I have my Expertise in server migration, manage SQL server , Manage windows based workstations and laptop in both hardware and software Level, Maintaining servers and securing servers with latest technology and keeping proper backups of important data. Also Have Matured Knowledge in microsoft office 2007/2010. I can also make PoS System for small business like cafe,food based stores. I also Have expertise in installing and configuring windows server and implementation of antivirus and able to troubleshoot any major issues with my troubleshooting skills. Please do check out my "CV" for more info about me..
+                </p>
+                <a href="#" class="text-base font-semibold text-white bg-red-500 py-3 px-8 rounded-full hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out">Download my CV</a>
+            </div>
+
+        </div>
+    </div>
+</template>
