e308ebb99b94a3d3a76b0fa03555b35052ce219c
LikhitaMagar01 <likhita.magar@patancollege.edu.np>
2022-10-09 19:21:05 +0545

    services grid-boxes

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -4,11 +4,43 @@
 <template>
     <div>
         <div class="bg-black">
-            <div class="grid grid-cols-1 gap-4 place-items-center">
-                <div class="text-2xl font-bold text-white">
+            <div class="grid grid-cols-1 place-items-center m-32">
+                <div class="text-2xl font-bold text-white mt-32">
                 Services
                 </div>
                 <span class="text-red-500">--what i provide--</span>
+            </div>
+            <div class="grid grid-rows-2 grid-flow-col gap-10 place-items-center m-10">
+                <div class="rounded bg-gray-700 hover:bg-red-500 p-10">
+                    <h1 class="text-white text-2xl font-bold">Web Design</h1>
+                    <p class="text-white">
+                        Making Static and Dynamic Website at most affordable price</p>
+                </div>
+                <div class="rounded bg-gray-700 hover:bg-red-500 p-10">
+                    <h1 class="text-white text-2xl font-bold">Web Design</h1>
+                    <p class="text-white">
+                        Making Static and Dynamic Website at most affordable price</p>
+                </div>
+                <div class="rounded bg-gray-700 hover:bg-red-500 p-10">
+                    <h1 class="text-white text-2xl font-bold">Web Design</h1>
+                    <p class="text-white">
+                        Making Static and Dynamic Website at most affordable price</p>
+                </div>
+                <div class="rounded bg-gray-700 hover:bg-red-500 p-10">
+                    <h1 class="text-white text-2xl font-bold">Web Design</h1>
+                    <p class="text-white">
+                        Making Static and Dynamic Website at most affordable price</p>
+                </div>
+                <div class="rounded bg-gray-700 hover:bg-red-500 p-10">
+                    <h1 class="text-white text-2xl font-bold">Web Design</h1>
+                    <p class="text-white">
+                        Making Static and Dynamic Website at most affordable price</p>
+                </div>
+                <div class="rounded bg-gray-700 hover:bg-red-500 p-10">
+                    <h1 class="text-white text-2xl font-bold">Web Design</h1>
+                    <p class="text-white">
+                        Making Static and Dynamic Website at most affordable price</p>
+                </div>
             </div>
             
         </div>
