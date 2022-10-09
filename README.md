4c43952d64543b7088d0ead1fa5c5c34de9ec553
LikhitaMagar01 <likhita.magar@patancollege.edu.np>
2022-10-09 09:16:47 +0545

    tailwindcss installation in vue

--- README.md
+++ README.md
@@ -5,3 +5,21 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+
+#vue+vite installation with yarn
+#vue create vite
+#yarn
+#yarn dev
+
+#tailwind installation in vue project
+#npm install -D tailwindcss postcss autoprefixer
+#npx tailwindcss init -p
+#intailwind.config.cjs file
+#insert inside content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+]
+#insert inside /src/style.css:
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,16 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="container mx-auto bg-red-200 rounded-xl shadow border p-8 m-10">
+     <p class="text-3xl text-black-700 font-bold mb-5">
+       Welcome!
+     </p>
+     <p class="text-gray-500 text-lg">
+       Vue and Tailwind CSS in action
+     </p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
